76176e0760e53422f184288563536cdc5606b095
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-08-29 22:37:16 +0530

    python questions

---
+++ setQuestions4/hardQues.py
@@ -0,0 +1,10 @@
+# 1.Find the union without using union().
+# 2.Find the intersection without using intersection().
+# 3.Find the difference without using difference().
+# 4.Remove duplicate words from a sentence using a set.
+# 5.Count the number of unique vowels in a string.
+# 6.Check whether two sets are equal.
+# 7.Create a set of squares from 1 to 10.
+# 8.Find the sum of all elements in a set.
+# 9.Print only even numbers from a set.
+# 10.Print only odd numbers from a set.
