1b7fbc1a0904cb008b01dedd968a901eba9f4a27
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-03 23:23:45 +0530

    update python files

---
+++ lesson4-dictionary & set/pracQues.py
@@ -0,0 +1,79 @@
+dictionary = {
+    "cat" : "a small animal",
+    "table" : "a small furniture or a piece of wood"
+}
+
+print(dictionary)
+print(dictionary.get("cat"))
+print(dictionary.get("table"))
+
+
+
+
+
+#--------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+subjects = {
+    "python","java","c++","python","javascript","java","python","C++","html"
+}
+
+print(len(subjects))
+
+
+
+
+
+
+#--------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+mark = {}
+
+X = int(input ("enter phy :"))
+mark.update({"phy" : X})
+
+
+X = int(input ("enter chem :"))
+mark.update({"chem" : X})
+
+
+X = int(input ("enter eng :"))
+mark.update({"eng" : X})
+
+
+X = int(input ("enter maths :"))
+mark.update({"maths" : X})
+
+
+print(mark)
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+val = {
+    ("float", 9.0),
+    ("int", 9)
+}
+
+print(val)
+
+
+
+
