903fd696f889bf60f88df71f28812b557c3938be
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-05 22:28:03 +0530

    update files

--- setQuestions4/mediumQues.py
+++ setQuestions4/mediumQues.py
@@ -1,5 +1,31 @@
 # 1.Find the union of two sets.
+
+
+s1 = {1,2,3,4}
+s2 = {5,6,7,4,3}
+
+print(s1.union(s2))
+
+
+
+#---------------------------------------------------------------------------------------------------------
+
+
+
 # 2.Find the intersection of two sets.
+
+
+set1 = {1,2,3,4,5}
+set2 = {2,3,4,5,7,8}
+
+print(set1.intersection(set2))
+
+
+
+#----------------------------------------------------------------------------------------------------------
+
+
+
 # 3.Find the difference between two sets.
 # 4.Find the symmetric difference of two sets.
 # 5.Remove duplicate elements from a list using a set.
