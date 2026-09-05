7bd5f5cda3cae692dbf4dbf2eca3745b4c7b54c7
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-05 23:39:48 +0530

    update files

---
+++ lesson5-Loops/loops.py
@@ -0,0 +1,31 @@
+          # while loop                          
+
+"""count = 0
+while count <= 5:
+    print("HELLO WORLD")               # print "hello world" 5 times
+    count += 1
+
+
+    print(count)"""
+
+
+
+
+ # print numbers from 1 to 10
+
+"""i = 1 
+while i <= 10:
+    print(i)
+    i += 1"""
+
+
+# reverse
+
+
+'''i = 10
+while i >=1 :
+    print(i)
+    i -= 1'''
+
+
+
