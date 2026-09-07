15de9de6a6ffda555e5c4804769cffd6f3ed84db
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-07 22:58:38 +0530

    update file

---
+++ lesson5-Loops/breakAndContinueStatement.py
@@ -0,0 +1,25 @@
+# break and continue statements....
+
+"""i = 1
+while i <= 5:
+    print(i)                          # break stopped the loop
+    if(i == 3):
+        break
+    i += 1
+print("end of the loop")"""
+
+
+
+
+
+i = 0
+while i <= 5:                       # break stopped the loop
+    if(i == 3):
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+
+
+ 
--- lesson5-Loops/practiceQuesforWhileLoop.py
+++ lesson5-Loops/practiceQuesforWhileLoop.py
@@ -39,7 +39,7 @@ while index < len(number):
 
 tup = (1,4,9,16,25,36,49,64,81,100)
 
-x = 25
+x = 36
 i= 0
 while i < len(tup):
     if (tup[i] == x):
