7cb2c5e2a124c9527109e6314e4f6b300a959862
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-12 22:29:00 +0530

    update file

--- lesson5-Loops/practiceQuesforWhileLoop.py
+++ lesson5-Loops/practiceQuesforWhileLoop.py
@@ -1,5 +1,7 @@
 # print numberss from 1 to 100 using while loop
 
+
+
 """i = 1
 while i <= 100:
     print(i)
@@ -7,6 +9,8 @@ while i <= 100:
 
 
 # print numbers from 100 to one using while loop
+
+
 
 """i = 100
 while i >= 1:
@@ -16,13 +20,17 @@ while i >= 1:
 
 # print multiplication table of a number n.
 
+
+
 """n = int(input("enter a number :"))
 i = 1                                                    # n is the number 
 while i <= 10:
     print(n*i)
     i += 1"""
 
+
 # print the elements of the following list using loops-[1,4,9,16,25,36,49,64,81,100]
+
 
 
 """number = [1,4,9,16,25,36,49,64,81,100]
@@ -35,6 +43,7 @@ while index < len(number):
 
 
 # search for a number X in this tuple using loops: (1,4,9,16,25,36,49,64,81,100)
+
 
 
 tup = (1,4,9,16,25,36,49,64,81,100)
