e30a8a32d16cfcebb63827b34ac6d295753ce41d
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-26 15:36:26 +0530

    update python file

--- lesson6-functions and recursion/functionInPython.py
+++ lesson6-functions and recursion/functionInPython.py
@@ -1,2 +1,40 @@
 #    functions in python
 
+'''def  Calc_sum(a,b):
+    s = a + b
+    print(s)
+    return(s)
+
+Calc_sum(5,6)
+
+
+
+
+def  Calc_sub(a,b):
+    sum = a - b
+    print(sum)
+    return(sum)
+
+Calc_sub(3,8)'''
+
+
+
+'''def calc_hell():
+    print("hello world")
+
+
+calc_hell() '''   
+
+
+
+
+# Average of 3 numbers
+
+
+
+def calc_avg(a,b,c):
+    Average = a + b + c / 3
+    print(Average)
+
+
+calc_avg(2,3,5)
